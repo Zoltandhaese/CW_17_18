308d4d6031c5428a04ca9f4450bc29f348656bb7
gillis <gillis@10.43.101.197>
2017-10-20 15:17:55 +0200

    Config

--- src/testbed/Main.java
+++ src/testbed/Main.java
@@ -1,9 +1,18 @@
 package testbed;
 
+import API.AutopilotConfig;
+
 public class Main {
 
 	public Main() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args){
+		AutopilotConfig config = new Config();
+		
+		
+		
 	}
 
 }
