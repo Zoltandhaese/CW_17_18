980e85185dca567c9a839b3e16ef41b563bc232d
Zoltan <zoltan.dhaese@student.kuleuven.be>
2017-10-20 12:51:45 +0200

    Second push

--- src/autopilot/Drone.java
+++ src/autopilot/Drone.java
@@ -2,7 +2,7 @@ package autopilot;
 
 public class Drone {
 
-	public Drone (double Xcoordinate,double Ycoordinate,double Xvelocity,double Yvelocity,double radius, double orientation, double mass)
+	public Drone (double Xcoordinate,double ,Ycoordinate,double Xvelocity,double Yvelocity,double radius, double orientation, double mass)
 	}
 	
 }
