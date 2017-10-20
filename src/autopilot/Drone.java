a453e006d65d65bc97bc58bfc182b4b2ca6749fe
Zoltan <zoltan.dhaese@student.kuleuven.be>
2017-10-20 12:58:28 +0200

    3de

--- src/autopilot/Drone.java
+++ src/autopilot/Drone.java
@@ -3,6 +3,6 @@ package autopilot;
 public class Drone {
 
 	public Drone (double Xcoordinate,double ,Ycoordinate,double Xvelocity,double Yvelocity,double radius, double orientation, double mass)
-	}
+	
 	
 }
